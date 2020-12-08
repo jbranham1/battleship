cf2bea4320aae0df2f7384319e572682f5f78795
Jenny Branham <branhamjenny@gmail.com>
2020-12-08 08:54:13 -0600

    Create cell test

---
+++ test/cell_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/cell'
+
+class CellTest < MiniTest::Test
+  def test_it_exists
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+  end
+end
