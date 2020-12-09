398abad794300ac839bea1c8155d600b41e6b9a4
Jenny Branham <branhamjenny@gmail.com>
2020-12-09 08:57:37 -0600

    Create test for valid coordinate

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,36 +5,22 @@ require './lib/cell'
 require './lib/ship'
 
 class BoardTest < MiniTest::Test
-
-  def setup
-    @board = Board.new
-    @expected_board = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4")
-    }
-  end
-
   def test_it_exists
-    assert_instance_of Board, @board
+    board = Board.new
+    assert_instance_of Board, board
   end
 
   def test_it_has_readable_attributes
-    assert_equal 16, @board.cells.count
-    assert_equal Hash, @board.cells.class
-    assert_equal Cell, @board.cells.values[1].class  
+    board = Board.new
+    assert_equal 16, board.cells.count
+    assert_equal Hash, board.cells.class
+    assert_equal Cell, board.cells.values[1].class
+  end
+
+  def test_valid_coordinate?
+    board = Board.new
+
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal false, board.valid_coordinate?("E1")
   end
 end
