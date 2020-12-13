9430f4778f17c6e11cbc4022812296764cdfbe70
Jenny Branham <branhamjenny@gmail.com>
2020-12-13 10:02:54 -0600

    Add game start to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,51 @@
 require "./lib/board_setup"
-
+require './lib/game_message'
+require 'pry'
 class Game
-  attr_reader :board_setup
+  attr_reader :game_message,
+              :computer_board,
+              :player_board,
+              :computer_cruiser,
+              :computer_sub,
+              :player_cruiser,
+              :player_sub
+
   def initialize
-    @board_setup = BoardSetup.new
+    @game_message = GameMessage.new
+    @computer_board = BoardSetup.new
+    @player_board = BoardSetup.new
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_sub = Ship.new("Submarine", 2)
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_sub = Ship.new("Submarine", 2)
+  end
+
+  def game_start
+    computer_place_ships
+    game_message.computer_board_placement(computer_board.board)
+    player_place_ships
+  end
+
+  def computer_place_ships
+    @computer_board.computer_place_ship(computer_cruiser)
+    @computer_board.computer_place_ship(computer_sub)
+  end
+
+  def player_place_ships
+    player_valid_entry(@player_cruiser)
+    player_valid_entry(@player_sub)
+  end
+
+  def player_valid_entry(ship)
+    game_message.player_ship_placement(ship.name, ship.length)
+    loop do
+      player_coordinates = gets.chomp.upcase.split
+      if !@player_board.player_place_ship(ship, player_coordinates).nil?
+        game_message.player_board_placement(@player_board.board)
+        break
+      else
+        game_message.invalid_input_for_coordinates
+      end
+    end
   end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/game'
 require './lib/board_setup'
+require './lib/ship'
 
 class GameTest < MiniTest::Test
   def test_it_exists
@@ -13,6 +14,11 @@ class GameTest < MiniTest::Test
   def test_it_has_readable_attributes
     game = Game.new
 
-    assert_instance_of BoardSetup ,game.board_setup
+    assert_instance_of BoardSetup, game.computer_board
+    assert_instance_of BoardSetup, game.player_board
+    assert_instance_of Ship, game.computer_cruiser
+    assert_instance_of Ship, game.computer_sub
+    assert_instance_of Ship, game.player_cruiser
+    assert_instance_of Ship, game.player_sub
   end
 end
