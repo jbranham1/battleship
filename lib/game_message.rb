29f4191cad29045600037e729934f99fc0c28444
Jenny Branham <branhamjenny@gmail.com>
2020-12-16 08:48:54 -0600

    Fix infinite ships

--- lib/game_message.rb
+++ lib/game_message.rb
@@ -3,8 +3,8 @@ class GameMessage
     puts "Please enter your board size. (Numbers 1-27)"
     loop do
       @number = gets.chomp.to_i
-      if @number > 27 || @number < 1
-        puts "Invalid board size. Please enter a number between 1 and 27."
+      if @number > 26 || @number < 4
+        puts "Invalid board size. Please enter a number between 4 and 26."
       else
         break
       end
